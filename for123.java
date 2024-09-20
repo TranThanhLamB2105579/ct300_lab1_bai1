dbe031329f32d8a267032d98eced1800d5272f27
TranThanhLam <lamb2105579@gmail.com.vn>
2024-09-20 20:14:54 +0700

    for123.java

---
+++ for123.java
@@ -0,0 +1,9 @@
+package ct300_lab1_bai1;
+
+public class for123 {
+    public static void main(String[] args){
+        for (int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
